e0e7d99eba3a3c92b96ce0a704f96febd793a14d
pisanuwat <pisanuwat.b@gmail.com>
2023-10-12 03:21:41 +0700

    -fix: update version number

--- lib/feature/profile/pages/profile_page.dart
+++ lib/feature/profile/pages/profile_page.dart
@@ -350,7 +350,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         S.h(10),
                         const Text(
-                          'SOLVE v 0.1.7',
+                          'SOLVE v 0.1.75',
                           style: TextStyle(
                             color: greyColor,
                           ),
