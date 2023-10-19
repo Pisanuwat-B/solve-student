9c65e0fe74539460b34e2885036a42565cbe1852
DOH <donote.44@gmail.com>
2023-10-19 20:31:20 +0700

    research replay.

--- lib/feature/replay/controller/replay_controller.dart
+++ lib/feature/replay/controller/replay_controller.dart
@@ -4,7 +4,8 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:solve_student/feature/replay/mock/replay_mock.dart';
 import 'package:solve_student/feature/replay/models/replay_model.dart';
-import 'package:solve_student/feature/replay/models/replay_offset_model.dart';
+import 'package:solve_student/feature/replay/models/replay_model.dart'
+    as replay;
 
 class ReplayController extends ChangeNotifier {
   ReplayController(this.context);
@@ -15,6 +16,7 @@ class ReplayController extends ChangeNotifier {
   int start = 0;
   num totalTime = 20;
   Offset? offsetPlay;
+  List<Offset?> offsetBuild = [];
 
   init() async {
     // startTimer();
@@ -22,9 +24,12 @@ class ReplayController extends ChangeNotifier {
   }
 
   setReplay() {
+    log("click");
+    start = 0;
+    offsetBuild = [];
     replayData = ReplayModel.fromJson(replayMock);
-    totalTime = 3000;
-    // totalTime = replayData?.actions?.last.time ?? 1;
+    // totalTime = 3000;
+    totalTime = replayData?.actions?.last.time ?? 1;
     startTimer();
     notifyListeners();
   }
@@ -34,17 +39,33 @@ class ReplayController extends ChangeNotifier {
     timer = Timer.periodic(
       oneSec,
       (Timer timer) {
-        log("run :$start");
         if (start >= totalTime) {
           timer.cancel();
           notifyListeners();
         } else {
-          // RunTime? thisTime = mock.where((element) {
-          //   return element.time == start;
-          // }).firstOrNull;
-          // if (thisTime != null) {
-          //   offsetPlay = Offset(thisTime.x!, thisTime.y!);
-          // }
+          replay.Action? findAction = replayData?.actions?.where((element) {
+            if (element.type == "drawing") {
+              DataClass setPoint = DataClass.fromJson(element.data);
+              for (var i = 0; i < setPoint.points!.length; i++) {
+                replay.Datum onlyPoint = setPoint.points![i];
+                if (onlyPoint.time == start) {
+                  log("point run : ${onlyPoint.toJson()}");
+                  offsetPlay = Offset(
+                    onlyPoint.x?.toDouble() ?? 0,
+                    onlyPoint.y?.toDouble() ?? 0,
+                  );
+                  offsetBuild.add(offsetPlay);
+                  notifyListeners();
+                  return true;
+                }
+              }
+            }
+            return false;
+          }).firstOrNull;
+          // log("action this time :$start");
+          if (findAction == null) {
+            offsetPlay = null;
+          }
           start = start + 1;
           notifyListeners();
         }
--- lib/feature/replay/pages/replay_page.dart
+++ lib/feature/replay/pages/replay_page.dart
@@ -31,31 +31,82 @@ class _ReplayPageState extends State<ReplayPage> {
           appBar: AppBar(
             title: const Text("Replay"),
           ),
-          body: Container(
-            height: Sizer(context).h,
-            width: Sizer(context).w,
-            child: SafeArea(
-              child: Stack(
-                children: <Widget>[
-                  Text("data : ${con.start}, ${con.offsetPlay}"),
-                  Builder(builder: (context) {
-                    if (con.offsetPlay == null) {
-                      return const SizedBox();
-                    }
-                    return AnimatedPositioned(
-                      duration: Duration(milliseconds: 300),
-                      top: con.offsetPlay!.dy - 100,
-                      left: con.offsetPlay!.dx - 100,
-                      child: Container(
-                        color: Colors.cyan,
-                        height: 100,
-                        width: 100,
-                        child: Text("hello "),
-                      ),
-                    );
-                  }),
+          body: SafeArea(
+            child: Container(
+              height: Sizer(context).h,
+              width: Sizer(context).w,
+              child: Column(
+                children: [
+                  const SizedBox(height: 100),
+                  Container(
+                    height: con.replayData?.solvepadHeight ?? 0,
+                    width: con.replayData?.solvepadWidth ?? 0,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                    ),
+                    child: Stack(
+                      children: <Widget>[
+                        Center(
+                          child: Image.network(
+                              "https://firebasestorage.googleapis.com/v0/b/solve-f1778.appspot.com/o/medias%2F3Bt8lonPd8SPqcLmCd3DR3onlln1%2F7nHrkvZwlb1ioATrFQEg%2F7nHrkvZwlb1ioATrFQEg_1690225529939.jpg?alt=media&token=1690225574769"),
+                        ),
+                        ...con.offsetBuild.map<Widget>((item) {
+                          return Positioned(
+                            top: item!.dy,
+                            left: item.dx,
+                            child: Container(
+                              height: 20,
+                              width: 20,
+                              color: Colors.red,
+                              alignment: Alignment.center,
+                            ),
+                          );
+                        }).toList(),
+                        
+                        Builder(builder: (context) {
+                          if (con.offsetPlay == null) {
+                            return const SizedBox();
+                          }
+                          return AnimatedPositioned(
+                            duration: Duration(milliseconds: 300),
+                            top: con.offsetPlay!.dy,
+                            left: con.offsetPlay!.dx,
+                            child: Container(
+                              color: Colors.cyan,
+                              height: 20,
+                              width: 20,
+                              child: Text("hello "),
+                            ),
+                          );
+                        }),
+                        Text("data : ${con.start}, ${con.offsetPlay}"),
+                      ],
+                    ),
+                  ),
                 ],
               ),
+            ),
+          ),
+          bottomNavigationBar: SafeArea(
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    con.timer!.cancel();
+                  },
+                  icon: Icon(
+                    Icons.pause,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    con.setReplay();
+                  },
+                  icon: Icon(
+                    Icons.play_arrow,
+                  ),
+                ),
+              ],
             ),
           ),
         );
