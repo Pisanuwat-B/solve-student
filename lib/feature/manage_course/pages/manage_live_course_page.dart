eed6b040e9dd16344971c3ccce072a76e9fd4197
pisanuwat <pisanuwat.b@gmail.com>
2023-11-10 10:49:29 +0700

    -fix: reduce responsive mobile frmo 850 to 650

--- lib/feature/manage_course/pages/manage_live_course_page.dart
+++ lib/feature/manage_course/pages/manage_live_course_page.dart
@@ -565,7 +565,7 @@ class _ManageLiveCoursePageState extends State<ManageLiveCoursePage> {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: const EdgeInsets.all(30),
+        padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(10),
@@ -581,12 +581,11 @@ class _ManageLiveCoursePageState extends State<ManageLiveCoursePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: SizedBox(
-                child: Image.asset(
-                  image,
-                  fit: BoxFit.contain,
-                ),
+            SizedBox(
+              height: 60,
+              child: Image.asset(
+                image,
+                fit: BoxFit.contain,
               ),
             ),
             const SizedBox(height: 20),
@@ -602,7 +601,7 @@ class _ManageLiveCoursePageState extends State<ManageLiveCoursePage> {
                           style: const TextStyle(
                             color: appTextPrimaryColor,
                             fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                            fontSize: 15,
                           ),
                           maxLines: 2,
                           overflow: TextOverflow.ellipsis,
