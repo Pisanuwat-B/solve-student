690219a352cf5aaf8b526baa88acc4cacbee2095
pisanuwat <pisanuwat.b@gmail.com>
2023-11-11 21:23:15 +0700

    -fix: review eraser adjust fields

--- lib/feature/profile/pages/profile_page.dart
+++ lib/feature/profile/pages/profile_page.dart
@@ -350,7 +350,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         S.h(10),
                         const Text(
-                          'SOLVE v 0.2.91',
+                          'SOLVE v 0.2.92',
                           style: TextStyle(
                             color: greyColor,
                           ),
